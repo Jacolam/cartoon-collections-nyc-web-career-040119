0fd331e74aeaf75b71f79885c914bdfc902138ce
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-18 15:28:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 def find_the_cheese(will_it_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
-    will_it_cheese.find { |is_this_cheese| cheese == is_this_cheese}
+    will_it_cheese.include? 
   end 
  
 end
