f866debae6bafe7363caddc56b9afbefdec49e7a
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-18 15:14:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+   dwarf_names.each_with_index do |dwarf , index|
+     puts "#{index+1} #{dwarf}"
+   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer)
+  
+  planeteer.collect do |element|
+    element.capitalize + "!"
+  end 
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
+  
 end
 
 def find_the_cheese# code an argument here
